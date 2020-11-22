67ec5e1c99ddfc78d31afcd36e20a7676aa63765
Edtlsoft <edwardlopez2824@gmail.com>
2020-11-21 22:04:28 -0500

    [add] Jetstream:inertia

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,119 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Personal profesional</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <!-- This example requires Tailwind CSS v2.0+ -->
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Id</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Opciones</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Nombres y Apellidos</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Cedula</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Fecha de nacimiento</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Profesion</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Dirección</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Municipio</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Telefono</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Sexo</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Vehiculo</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Marca</th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Año</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <div class="flex items-center">
+                                                        <div class="text-sm font-medium text-gray-900">Jane Cooper</div>
+                                                    </div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <div class="flex justify-between">
+                                                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                            <i class="fas fa-edit"></i> Editar
+                                                        </button>
+                                                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                            Eliminar
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
+                                            </tr>
+
+                                            <!-- More rows... -->
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import Welcome from "@/Jetstream/Welcome";
+
+export default {
+  components: {
+    AppLayout,
+    Welcome,
+  },
+  // props: {
+  //     profesionales: {
+  //         type: Object,
+  //         required: true,
+  //     }
+  // }
+};
+</script>
